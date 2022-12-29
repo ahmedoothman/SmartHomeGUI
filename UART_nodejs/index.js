463b1b11694e76c980f3d566165d139e9211e7fc
Ahmed Othman <ahmed.othman.ghazzawi@gmail.com>
2022-12-29 12:58:16 +0200

    commented log weather

--- UART_nodejs/index.js
+++ UART_nodejs/index.js
@@ -20,7 +20,7 @@ const getWeather = async () => {
   const response = await superagent.get(
     'http://api.weatherapi.com/v1/current.json?key=1e2e153b52cc4f3ca55212643222812&q=Cairo&aqi=no'
   );
-  console.log(response.body.current);
+  // console.log(response.body.current);
   let temp = response.body.current.temp_c;
   let hum = response.body.current.humidity;
   statusObj.outdoorTemp = temp + '°';
